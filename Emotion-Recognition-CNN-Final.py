0a38d7bcb501d9ef93f675af0b706b4c37d04297
maddoxrb <maddox.barron31@gmail.com>
2023-06-09 12:06:44 -0400

    Final Test Accuracy = 52.06% Peak Test Accuracy = 52.06% Changes: Added weight balances to correct for some classes having less sample size Issues: Requires more epochs, pehaps relu should be switched out with a softmax function? Still need to toy with learning rate, maybe use a new scheduler specific to plateaus

--- Emotion-Recognition-CNN-Final.py
+++ Emotion-Recognition-CNN-Final.py
@@ -17,6 +17,7 @@ class CustomDataset(Dataset):
         self.image_files = []
         self.labels = []
         self.transform = transform
+        self.classes = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
 
         # Traverse through the subfolders to collect image paths and labels
         for label in os.listdir(data_dir):
@@ -38,21 +39,9 @@ class CustomDataset(Dataset):
             image = self.transform(image)
 
         label = self.labels[idx]
-        if label == 'angry':
-            label = 0
-        if label == 'disgust':
-            label = 1
-        if label == 'fear':
-            label = 2
-        if label == 'happy':
-            label = 3
-        if label == 'neutral':
-            label = 4
-        if label == 'sad':
-            label = 5
-        if label == 'surprise':
-            label = 6
+        label = self.classes.index(label)
         return image, label
+
 
 # Specify the data transformations
 train_transform = transforms.Compose([
@@ -75,6 +64,18 @@ test_data_dir = 'C:\\Users\\Maddox\\Downloads\\archive (3)\\test'
 train_dataset = CustomDataset(data_dir=train_data_dir, transform=train_transform)
 test_dataset = CustomDataset(data_dir=test_data_dir, transform=test_transform)
 
+# Compute class weights
+class_weights = []
+total_samples = len(train_dataset)
+num_classes = len(train_dataset.classes)
+for label in train_dataset.classes:
+    count = train_dataset.labels.count(label)
+    weight = total_samples / (num_classes * count)
+    class_weights.append(weight)
+
+# Convert class weights to tensor
+class_weights = torch.tensor(class_weights, dtype=torch.float)
+
 # Create data loaders
 batch_size = 64
 train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
@@ -85,98 +86,72 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)  # Batch normalization after the first convolutional layer
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)  # Batch normalization after the second convolutional layer
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(128)  # Batch normalization after the third convolutional layer
-        self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(128 * 6 * 6, 256)  # First fully connected layer
-        self.dropout1 = nn.Dropout(0.5)  # Dropout layer after the first fully connected layer
-        self.fc2 = nn.Linear(256, 128)  # Second fully connected layer
-        self.dropout2 = nn.Dropout(0.5)  # Dropout layer after the second fully connected layer
-        self.fc3 = nn.Linear(128, 64)  # Third fully connected layer
-        self.dropout3 = nn.Dropout(0.5)  # Dropout layer after the third fully connected layer
-        self.fc4 = nn.Linear(64, 7)  # Output size is 7 for the number of emotions
+        self.fc1 = nn.Linear(128 * 6 * 6, 512)
+        self.fc2 = nn.Linear(512, 7)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        x = self.conv3(x)
-        x = self.bn3(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
+        x = nn.functional.relu(self.conv1(x))
+        x = nn.functional.max_pool2d(x, 2)
+        x = nn.functional.relu(self.conv2(x))
+        x = nn.functional.max_pool2d(x, 2)
+        x = nn.functional.relu(self.conv3(x))
+        x = nn.functional.max_pool2d(x, 2)
         x = x.view(x.size(0), -1)
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.dropout1(x)  # Apply dropout after the first fully connected layer
+        x = nn.functional.relu(self.fc1(x))
         x = self.fc2(x)
-        x = self.relu(x)
-        x = self.dropout2(x)  # Apply dropout after the second fully connected layer
-        x = self.fc3(x)
-        x = self.relu(x)
-        x = self.dropout3(x)  # Apply dropout after the third fully connected layer
-        x = self.fc4(x)
         return x
 
+# Initialize the CNN model
 model = CNN()
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.0001, weight_decay=0.0001)
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+# Define the loss function and optimizer
+criterion = nn.CrossEntropyLoss(weight=class_weights)
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
+# Training loop
 num_epochs = 15
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
-
 for epoch in range(num_epochs):
-    print("Processing Epoch " + str(epoch) + ": ....")
-    model.train()
+    # Training
     train_loss = 0.0
-
+    model.train()
     for images, labels in train_data_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-
         optimizer.zero_grad()
+
+        # Forward pass
         outputs = model(images)
         loss = criterion(outputs, labels)
+
+        # Backward and optimize
         loss.backward()
         optimizer.step()
 
         train_loss += loss.item() * images.size(0)
 
-    train_loss = train_loss / len(train_dataset)
+    train_loss /= len(train_dataset)
 
-    model.eval()
+    # Evaluation
     test_loss = 0.0
-    correct = 0
-
+    test_accuracy = 0.0
+    model.eval()
     with torch.no_grad():
         for images, labels in test_data_loader:
-            images = images.to(device)
-            labels = labels.to(device)
 
+            # Forward pass
             outputs = model(images)
             loss = criterion(outputs, labels)
+
             test_loss += loss.item() * images.size(0)
 
+            # Calculate accuracy
             _, predicted = torch.max(outputs, 1)
-            correct += (predicted == labels).sum().item()
+            correct_predictions = (predicted == labels).sum().item()
+            test_accuracy += correct_predictions
 
-    test_loss = test_loss / len(test_dataset)
-    test_accuracy = correct / len(test_dataset) * 100
+        test_loss /= len(test_dataset)
+        test_accuracy /= len(test_dataset)
+        test_accuracy *= 100
 
-    scheduler.step()  # Update learning rate scheduler
-
-    print(f'Epoch {epoch+1}/{num_epochs}: Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.2f}%')
-
-torch.save(model.state_dict(), 'emotion_classifier.pth')
+    print(f"Epoch {epoch+1}/{num_epochs}: Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.2f}%")
