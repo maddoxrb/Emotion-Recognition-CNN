e6e2de5481415778c1e9799533c96d2141a80534
A. Shrey <aditya.shrey4@gmail.com>
2023-06-12 10:43:17 -0700

    Navigation Bar Feature

---
+++ my-app/src/NavBar.js
@@ -0,0 +1,67 @@
+import { ChakraProvider, Box, Flex, Spacer, Link, Button, Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, Text, DrawerCloseButton, useDisclosure, useColorMode, IconButton } from "@chakra-ui/react";
+import { ChevronDownIcon, HamburgerIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+
+function NavBar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <ChakraProvider>
+      <Box bg={colorMode === "dark" ? "gray.900" : "gray.200"} p={4}>
+        <Flex maxW="1200px" mx="auto" align="center">
+          <Box>
+            <Text as='b' fontSize='3xl'>
+              Face Emotion Detector
+            </Text>
+          </Box>
+          <Spacer />
+          <Box display={{ base: "none", md: "block" }} mr={-2}>
+            <Link href="Model" mx={2} color={colorMode === "dark" ? "white" : "gray.500"} _hover={{ fontWeight: "bold" }}>
+              Model Accuracy Testing
+            </Link>
+            <Link href="" mx={2} color={colorMode === "dark" ? "white" : "gray.500"} _hover={{ fontWeight: "bold" }}>
+              Info
+            </Link>
+          </Box>
+          <Box display={{ base: "block", md: "none" }}>
+            <Button onClick={onOpen} size="sm" variant="ghost" color={colorMode === "dark" ? "white" : "gray.500"}>
+              <HamburgerIcon />
+            </Button>
+          </Box>
+          <IconButton
+            aria-label="Toggle dark mode"
+            icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+            onClick={toggleColorMode}
+            ml={2}
+            color={colorMode === "dark" ? "white" : "gray.500"}
+          />
+        </Flex>
+      </Box>
+      <Box bg="blue.200" height="10px" />
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Menu</DrawerHeader>
+            <DrawerBody>
+              <Link href="#" display="block" my={2} color={colorMode === "dark" ? "white" : "gray.500"} _hover={{ fontWeight: "bold" }}>
+                Model Accuracy Testing
+              </Link>
+              <Link href="#" display="block" my={2} color={colorMode === "dark" ? "white" : "gray.500"} _hover={{ fontWeight: "bold" }}>
+                Info
+              </Link>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </ChakraProvider>
+  );
+}
+
+export default NavBar;
